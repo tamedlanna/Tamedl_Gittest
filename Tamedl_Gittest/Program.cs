ed38dae76ea90b41af8aeff42dea492c139ec295
Kathrin Walker <Kathrin Walker@DESKTOP-KL30K26>
2017-09-29 13:00:14 +0200

    Geloest!!!

--- Tamedl_Gittest/Program.cs
+++ Tamedl_Gittest/Program.cs
@@ -10,11 +10,9 @@ namespace Tamedl_Gittest
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             //Ein Change für Kathrin :D
-=======
+
             //Kathrin hat jetzt was geschrieben :D
->>>>>>> 2d4295dfa8b95c5267eae64285970e8e542d4b67
 
             //TEEESSSTT
         }
