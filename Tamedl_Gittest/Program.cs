d149fa09eb11359db0b8307bd4d297e9afc48858
Matthias Zarl <matthias.zarl@gmail.com>
2017-10-06 13:02:48 +0200

    DUSCHEN!!!!

--- Tamedl_Gittest/Program.cs
+++ Tamedl_Gittest/Program.cs
@@ -12,6 +12,7 @@ namespace Tamedl_Gittest
         {
             //Ein Change für Kathrin :D
             //Anna stinkt. LG Clemens
+            //geh amoi duschen du dreckige!!!!
             //Kathrin hat jetzt was geschrieben :D
             //eric is scheiße
             //TEEESSSTT
